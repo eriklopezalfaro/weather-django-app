c559c57ab5f21b65653ed446d98d6770d6a83da3
Erik Lopez <eriklopezalfaro@outlook.com>
2022-11-08 12:11:11 -0800

    Set up API keys and HTTPresponse object, made sure everything is set up correctly.

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
+from django.http import HttpResponse
 
 # Create your views here.
+def main(requests):
+    print('RUnning...')
+    return HttpResponse('HELLO')
+
+if __name__ == '__main__':
+    main()
